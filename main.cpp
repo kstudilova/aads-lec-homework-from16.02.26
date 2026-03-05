46de2b48146c77a6a0acf6b750c1f1b54c2c5ef9
kstudilova <kstudilova@gmail.com>
2026-03-05 21:46:25 +0300

    implementation of a simplified interface

--- main.cpp
+++ main.cpp
@@ -1,42 +1,104 @@
 #include <iostream>
 
 template< class T >
-struct BiList {
-    T val;
-    BiList< T >* next;
-    BiList< T >* prev;
+struct BiList
+{
+  T val;
+  BiList< T >* next;
+  BiList< T >* prev;
 }; 
 
 template< class T >
-BiList< T >* add(BiList< T >* h, const T& d) {
-
+BiList< T >* add(BiList< T >* h, const T& d)
+{
+  BiList< T >* newNode = new BiList< T >{d, h, h->prev};
+  h->prev->next = newNode;
+  h->prev = newNode;
+  return newNode;
 };
 
 template< class T >
-BiList< T >* insert(BiList< T >* h, const T& d) {
-
+BiList< T >* insert(BiList< T >* h, const T& d)
+{
+  BiList< T >* newNode = new BiList< T >{d, h->next, h};
+  h->next->prev = newNode;
+  h->next = newNode;
+  return newNode;
 };
 
 template< class T >
-BiList< T >* cut(BiList< T >* h) noexcept {
-
+BiList< T >* cut(BiList< T >* h) noexcept
+{
+  if (h->next == h) 
+  {
+    delete h;
+    return nullptr;
+  }
+  BiList< T >* newHead = h->next;
+  h->prev->next = h->next;
+  h->next->prev = h->prev;
+  delete h;
+  return newHead;
 };
 
 template< class T >
-BiList< T >* erase(BiList< T >* h) noexcept {
-
+BiList< T >* erase(BiList< T >* h) noexcept
+{
+  if (h->next == h)
+  {
+    return h;
+  }
+  BiList< T >* toDelete = h->next;
+  h->next = toDelete->next;
+  toDelete->next->prev = h;
+  delete toDelete;
+  return h;
 };
 
 template< class T >
-BiList< T >* clear(BiList< T >* h, BiList< T >* e) noexcept {
-
+BiList< T >* clear(BiList< T >* h, BiList< T >* e) noexcept
+{
+  if (h == e)
+  {
+    delete h;
+    return nullptr;
+  }
+  BiList< T >* current = h;
+  BiList< T >* next = nullptr;
+  while (current != e)
+  {
+    next = nurrent->next;
+    delete current;
+    current = next;
+  }
+  delete e;
+  return nullptr;
 };
 
 template< class T, class F >
-BiList< T >* erase(F f, BiList< T >* h, BiList< T >* e) noexcept {
-
+F traverse(F f, BiList< T >* h, BiList< T >* e) noexcept
+{
+  BiList< T >* current = h;
+  if (e == nullptr)
+  {
+    while (true)
+    {
+      f(current->val);
+      current = current->next;
+      if (current == h) break;
+    }
+  } else {
+    while (true) 
+    {
+      f(current->val);
+      if (current == e) break;
+      current = current->next;
+    }
+  }
+  return f;
 };
 
-int main() {
+int main()
+{
 
 }
